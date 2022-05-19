c04c8c4ceaab3467234efc853cf52e3fb3fb31c7
Henri Heinmäe <henriheinmae@gmail.com>
2022-05-19 22:14:13 +0300

    add new projects

---
+++ UserNameDateTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserNameDateTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
